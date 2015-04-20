56f0e6eb85ecf7af4258cfd9ecf7e832daef51fe
mathieu.baudonnat <m.baudonnat@yahoo.fr>
2015-04-20 20:58:49 +0200

    mise en for home page php + css

--- donation/page-home.php
+++ donation/page-home.php
@@ -41,14 +41,14 @@ get_header(); ?>
 		<section class="info">
 			<div class="center cf">
 				<div class="col-4 ">
-					<span class="icon fa fa-plus"></span>
+					<span class="icon fa fa-child"></span>
 					<div class="text">
 						<h3><?php echo ale_get_meta('infotitle1'); ?></h3>
 						<p><?php echo ale_get_meta('infotext1'); ?></p>
 					</div>
 				</div>
 				<div class="col-4">
-					<span class="icon fa fa-globe"></span>
+					<span class="icon fa fa-plus"></span>
 					<div class="text">
 						<h3><?php echo ale_get_meta('infotitle2'); ?></h3>
 						<p><?php echo ale_get_meta('infotext2'); ?></p>
@@ -56,7 +56,7 @@ get_header(); ?>
 
 				</div>
 				<div class="col-4">
-					<span class="icon fa fa-tint"></span>
+					<span class="icon fa fa-home"></span>
 					<div class="text">
 						<h3><?php echo ale_get_meta('infotitle3'); ?></h3>
 						<p><?php echo ale_get_meta('infotext3'); ?></p>
@@ -72,8 +72,8 @@ get_header(); ?>
 		<section class="donate_ homedonate">
 			<div class="cf">
 				<div class="col-6 text">
-					<h3><span class="fa fa-cubes"></span><?php _e('Thank you!','aletheme'); ?></h3>
-					<h4><?php _e("You've helped us raise a staggering","aletheme"); ?></h4>
+					<h3><?php _e('Merci!','aletheme'); ?></h3>
+					<h4><?php _e("Grace &agrave vous nous avons r&eacutecolt&eacute","aletheme"); ?></h4>
 					<h5>
 						<span>
 							<?php
@@ -85,13 +85,13 @@ get_header(); ?>
 							}
 							?>
 					   </span>
-						<?php _e('so far!','aletheme'); ?>
+						<?php _e('pour nos projets','aletheme'); ?>
 					</h5>
 				</div>
 
 				<?php if(shortcode_exists('dgx-donate')): ?>
 					<div class="col-6 but">
-						<a href="#" class="button-big"><span class="text"><?php _e('Donate/pay in your money','aletheme'); ?></span><span class="shadow"></span></a>
+						<a href="#" class="button-big"><span class="text"><?php _e('Aidez nous','aletheme'); ?></span><span class="shadow"></span></a>
 					</div>
 				<?php endif; ?>
 			</div>
@@ -236,76 +236,61 @@ get_header(); ?>
 			</div>
 		</section>
 	<?php endif; ?>
-
+	
+<section class="partenaires">
+		<div class="center cf">
+			<h2>Nos partenaires</h2>
+			<div >
+				<div class="col-2i">
+					<div class="partner-logo">
+						<a  target=blank href="http://www.sosprema.com"><img class="sosprema" alt="sosprema" src="http://localhost/aptitspas/wordpress/wp-content/themes/donation/css/images/logo-sosprema.png"></a>
+					</div>
+				</div>
+				<div class="col-2i">
+					<div class="partner-logo">
+						<a  target=blank href="http://www.lactalis.fr"><img class="lactalis"alt="lactalis" src="http://localhost/aptitspas/wordpress/wp-content/themes/donation/css/images/logo-lactalis.png"></a>
+					</div>
+				</div>
+				<div class="col-2i">
+					<div class="partner-logo">
+						<a  target=blank href="http://www.nestle.fr"><img class="nestle"alt="nestle" src="http://localhost/aptitspas/wordpress/wp-content/themes/donation/css/images/logo-nestle.png"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
 	<?php if(ale_get_meta('howhelphome')!='off'): ?>
 		<!-- How can you help -->
 		<section class="how-help">
 			<div class="top-border"></div>
 
 			<div class="center cf">
-				<h2><?php echo ale_get_meta('howhelptitle'); ?></h2>
+				<h2><img alt="aptitpas" src="http://localhost/aptitspas/wordpress/wp-content/themes/donation/css/images/logo-blanc.png"><?php echo ale_get_meta('howhelptitle'); ?></h2>
 
 				<div class="col-6 left">
 					<h3><?php echo ale_get_meta('howhelptitlesmall'); ?></h3>
 					<p>
 						<?php echo ale_get_meta('howhelptext'); ?>
 					</p>
-					<div class="col-6 item"><span class="fa fa-film"></span><?php echo ale_get_meta('howhelpmadia'); ?></div>
-					<div class="col-6 item"><span class="fa fa-shield"></span><?php echo ale_get_meta('howhelppro'); ?></div>
-					<div class="col-6 line"><div class="li"></div></div>
-					<div class="col-6 line"><div class="li"></div></div>
-					<div class="col-6 item"><span class="fa fa-users"></span><?php echo ale_get_meta('howhelpmob'); ?></div>
-					<div class="col-6 item"><span class="fa fa-dot-circle-o"></span><?php echo ale_get_meta('howhelpsup'); ?></div>
-					<a class="button" href="<?php echo ale_get_meta('howhelpurl'); ?>"><span class="text"><?php _e('Read More','aletheme'); ?></span><span class="shadow"></span></a>
+					<a class="button" href="<?php echo ale_get_meta('howhelpurl'); ?>"><span class="text"><?php _e('En savoir plus','aletheme'); ?></span><span class="shadow"></span></a>
 				</div>
 				<div class="col-6 right">
-					<?php if(ale_get_meta('howhelpvideo')): ?>
-						<?php echo wp_oembed_get(ale_get_meta('howhelpvideo'),array('width'=>'100%','height'=>'296px')); ?>
-					<?php else: echo '<img src="http://placehold.it/500x296/f2f2f2/414141&amp;text=Place+for+video" alt>'; endif; ?>
+					<div class="col-6 item"><a href="" class="howhelplink"> Devenir Membre </a></div>
+					<div class="col-6 item"><a href="" class="howhelplink"> Devenir M&eacutec&egravene </a></div>
+					<div class="col-6 item"><a href="" class="howhelplink"> Devenir B&eacuten&eacutevole </a></div>
+					
+					
 				</div>
 			</div>
 		</section>
 	<?php endif; ?>
 
 	<?php if(ale_get_meta('newshome')!='off'): ?>
-		<!-- How can you help -->
+		<!-- About and Gallery -->
 		<section class="news">
 			<div class="center cf">
-				<div class="col-3 news">
-					<h3><?php echo ale_get_meta('newstitle'); ?></h3>
-					<?php
-					wp_reset_query();
-					$query_news = new WP_Query(
-						array(
-							'posts_per_page' => 2,
-							'post_type' => 'news',
-							'ignore_sticky_posts' => 1,
-							'post__not_in' => get_option('sticky_posts')
-						)
-					);
-					if ($query_news->have_posts()) : while ($query_news->have_posts()) : $query_news->the_post(); ?>
-						<article>
-							<div class="time"><span class="fa fa-clock-o"></span><?php echo ale_get_meta('starttime'); ?><?php if(ale_get_meta('endtime')){ _e(' - ','aletheme');} echo ale_get_meta('endtime'); ?></div>
-							<h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
-							<?php echo ale_trim_excerpt(14); ?>
-
-							<div class="info cf">
-								<div class="reply"><span class="fa fa-mail-forward"></span><?php echo get_post_views(get_the_ID()); ?></div>
-								<div class="comment"><span class="fa fa-comment-o"></span><?php $comments_count = wp_count_comments($post->ID); echo $comments_count->total_comments; ?></div>
-								<?php
-								$current_category = wp_get_post_terms($post->ID, 'news-category', array("fields" => "all"));
-								foreach($current_category as $curcat){
-									echo '<div class="tag">';
-									echo $curcat->name.' ';
-									echo '</div>';
-								}
-								?>
-							</div>
-						</article>
-					<?php endwhile;  endif; wp_reset_query();?>
-
-					<a class="button" href="<?php echo get_post_type_archive_link('news');?>"><span class="text"><?php _e('More news','aletheme'); ?></span><span class="shadow"></span></a>
-				</div>
+				
 				<div class="col-6 about">
 					<h3><?php echo ale_get_meta('aboutustitle'); ?></h3>
 					<?php if(ale_get_meta('aboutusimage')): ?>
@@ -361,23 +346,23 @@ get_header(); ?>
 			<div class="center cf">
 				<?php
 				wp_reset_query();
-				$query_events = new WP_Query(
+				$query_news = new WP_Query(
 					array(
 						'posts_per_page' => 3,
-						'post_type' => 'events',
+						'post_type' => 'news',
 						'ignore_sticky_posts' => 1,
 						'post__not_in' => get_option('sticky_posts')
 					)
 				);
-				if ($query_events->have_posts()) : while ($query_events->have_posts()) : $query_events->the_post(); $count++; ?>
+				if ($query_news->have_posts()) : while ($query_news->have_posts()) : $query_news->the_post(); $count++; ?>
 				<div class="col-4">
 					<div class="img">
-						<?php if(get_the_post_thumbnail($post->ID,'events-medium')): ?>
-							<?php echo get_the_post_thumbnail($post->ID,'events-medium');?>
+						<?php if(get_the_post_thumbnail($post->ID,'news-medium')): ?>
+							<?php echo get_the_post_thumbnail($post->ID,'news-medium');?>
 						<?php else: echo '<img src="http://placehold.it/330x194/f2f2f2/414141&amp;text=No+image" alt>'; endif; ?>
 						<div class="text">
 							<h3><?php the_title();?></h3>
-							<a href="<?php the_permalink();?>"><?php _e('Read more','aletheme'); ?></a>
+							<a href="<?php the_permalink();?>"><?php _e('Lire la suite','aletheme'); ?></a>
 						</div>
 					</div>
 				</div>
